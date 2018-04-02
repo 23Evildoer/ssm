f5592db7b5aa387c9288acf787954c7d6721b3ab
jiaoyeye <875804969@qq.com>
2018-04-02 16:15:15 +0800

    Initial commit

---
+++ spring/src/main/java/ssm/com/pojo/Smm.java
@@ -0,0 +1,13 @@
+package ssm.com.pojo;
+
+public class Smm {
+    private  String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
